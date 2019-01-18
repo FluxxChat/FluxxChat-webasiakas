918e12ecfeb4613b182628090c9dc78fb3c89c29
pyigyli <pyry.vahakangas@luukku.com>
2019-01-18 14:50:32 +0200

    chore: username form added

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -4,6 +4,12 @@ import ReactDOM from 'react-dom';
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-	<div>Hello World!</div>,
+	<div>
+		<form>
+			Username:
+			<input type="text"/>>
+			<input type="submit" value="Continue"/>>
+		</form>
+	</div>,
 	rootElement
 );
