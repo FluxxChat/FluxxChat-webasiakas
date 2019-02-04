f30b125af5b5db8a3495389e51810927f9bf4c58
pyigyli <pyry.vahakangas@luukku.com>
2019-02-04 15:00:11 +0200

    style: multi-language options added

--- src/client/components/CardParameters.tsx
+++ src/client/components/CardParameters.tsx
@@ -35,7 +35,7 @@ export class PlayerParameter extends React.Component<PlayerParameterProps> {
 					onChange={this.props.onChange}
 					className="select_rule_target"
 				>
-					<option value="-1">Valitse kohde</option>
+					<option value="-1"/>>
 					{this.props.users.map(user => (
 						<option key={user.id} value={user.id}>{user.nickname}</option>
 					))}
